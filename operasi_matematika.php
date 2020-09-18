d918bb2d7e0eb1057e4e22e7d689b069b8e18d18
matsnahr <matsnahr01@gmail.com>
2020-09-17 18:59:12 -0700

    tambah operasi perkalian

--- operasi_matematika.php
+++ operasi_matematika.php
@@ -29,4 +29,7 @@ if($pilih_operasi==1)
 }else if($pilih_operasi==2){
     $hasil =$bil_satu - $bil_kedua;
     echo $template_hasil.$hasil;
+}else if($pilih_operasi==3){
+    $hasil =$bil_satu * $bil_kedua;
+    echo $template_hasil.$hasil;
 }
